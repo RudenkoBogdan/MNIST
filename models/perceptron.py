80e2c34920318d926c472d08601855130090e005
RudenkoBogdan <rudenko.ba03@gmail.com>
2026-04-03 20:48:46 +0300

    #2 feat(perceptron): the basic structure of the class has been defined

---
+++ models/perceptron.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Perceptron:
+    def __init__():
+
+    def _init_weights():
+
+    def forward():
+
+    def _softmax():
+
+    def _cross_entropy_loss():
+
+    def backward():
+
+    def fit():
+
+    def predict():
+
+    def predict_proba():
+    
+    def evaluate():
