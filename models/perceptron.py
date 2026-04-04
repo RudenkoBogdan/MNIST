0883d91367f19af43be47322d5ee963c813243ab
RudenkoBogdan <rudenko.ba03@gmail.com>
2026-04-04 16:47:11 +0300

    #3 feat(perceptron): internal parameters have been added to init

--- models/perceptron.py
+++ models/perceptron.py
@@ -1,7 +1,22 @@
 import numpy as np
 
 class Perceptron:
-    def __init__():
+    def __init__(
+            self,
+            input_size = 784,
+            n_classes = 10,
+            learning_rate = 1e-3,
+            epochs = 30,
+            batch_size = 64,
+            random_seed =42):
+        
+        self.input_size = input_size
+        self.n_classes = n_classes
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.batch_size = batch_size
+
+        self.rng = np.random.default_rng(random_seed)
 
     def _init_weights():
 
